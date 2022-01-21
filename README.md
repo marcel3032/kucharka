b13987898af1d203f0c844d57388ea7f9a455318
Marcel Palaj <palaj.marcel@gmail.com>
2022-01-21 18:55:27 +0100

    encoding change

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Simple tex template for cooking recipes
 - add `recipe_name.tex` to `/recipes`
 - add recipe to `main.tex`
 - build
-	- `make.bat` on Windows
+	- `make.bat` on Windows (using `OEM 852` encoding)
 	- or build manually:
 		- build pdf e.g. (`pdfLaTeX main.tex`)
 		- build html e.g. (`pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuchárka"`)
--- make.bat
+++ make.bat
@@ -1,8 +1,10 @@
+REM ## WARNING: Using OEM 852 encoding to work in w7 cmd.exe ##
+
 pdfLaTeX main.tex
 pdfLaTeX main.tex
 
 REM htlatex main.tex "xhtml,html5,index=3,charset=utf-8" " -cunihtf -utf8"
 REM htlatex main.tex "xhtml,html5,index=3,charset=utf-8" " -cunihtf -utf8"
 
-pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch√°rka"
-pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch√°rka"
+pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch†rka"
+pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch†rka"
