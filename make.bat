3f2d7e0f5a8a8b005bfc9f6e4b574e70eba82362
Marcel Palaj <palaj.marcel@gmail.com>
2022-01-21 20:40:00 +0100

    start styling html

--- make.bat
+++ make.bat
@@ -6,5 +6,5 @@ pdfLaTeX main.tex
 REM htlatex main.tex "xhtml,html5,index=3,charset=utf-8" " -cunihtf -utf8"
 REM htlatex main.tex "xhtml,html5,index=3,charset=utf-8" " -cunihtf -utf8"
 
-pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch rka"
-pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch rka"
+pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch rka" -c style.css
+pandoc main.tex -s --toc -t html -o main.html --metadata title="Kuch rka" -c style.css
