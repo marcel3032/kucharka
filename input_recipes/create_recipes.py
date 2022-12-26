8462f56beb1ef80ecc67e3f35c3a5cbbb806acbf
Marcel Palaj <palaj.marcel@gmail.com>
2022-12-26 14:13:54 +0100

    add json schema for yml

--- input_recipes/create_recipes.py
+++ input_recipes/create_recipes.py
@@ -1,6 +1,8 @@
 import jinja2
 import yaml
 import os
+import jsonschema
+import json
 
 recipes_dir = 'recipes'
 out_dir = '..'+os.sep+'output_recipes'
@@ -18,5 +20,7 @@ for dir in list(os.listdir(recipes_dir)):
 
     assert recipe_filename[-4:]=='.yml'
     values = yaml.safe_load(open(recipe_filename, 'r', encoding='utf-8'))
+    schema = json.load(open('recipe-schema.json', 'r'))
+    jsonschema.validate(instance=values, schema=schema)
     with open(out_dir+os.sep+dir+os.sep+recipe[:-4]+tex_filname_extension, 'w', encoding='utf-8') as fout:
       print(template.render(values), file=fout)
